e6d443ed5248b2fccd71214f60531fb5d01fb145
Henrique Biondo Nicolli Soares <henriquenicolli@Henriques-MacBook-Air.local>
2022-12-27 21:29:59 -0300

    add products register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListQuotesComponent } from './list-quotes/list-quotes.component';
 import { RegisterQuotesComponent } from './register-quotes/register-quotes.component';
+import { RegisterProductsComponent } from './register-products/register-products.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ListQuotesComponent,
-    RegisterQuotesComponent
+    RegisterQuotesComponent,
+    RegisterProductsComponent
   ],
   imports: [
     BrowserModule
