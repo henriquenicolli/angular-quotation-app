bc5de5dca03932b873d96ff0cea569ef9eb459d8
Henrique Biondo Nicolli Soares <henriquenicolli@Henriques-MacBook-Air.local>
2023-01-01 18:29:30 -0300

    add navigation pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ListQuotesComponent } from './list-quotes/list-quotes.component';
@@ -8,11 +8,17 @@ import { RegisterQuotesComponent } from './register-quotes/register-quotes.compo
 import { RegisterProductsComponent } from './register-products/register-products.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {HttpClientModule} from '@angular/common/http';
-import {MaterialExampleModule} from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialExampleModule } from './material.module';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: 'listaCotacoes', component: ListQuotesComponent },
+  { path: 'cadastrarCotacao', component: RegisterQuotesComponent },
+  { path: 'cadastrarProduto', component: RegisterProductsComponent }
+];
 
 @NgModule({
   declarations: [
@@ -29,7 +35,8 @@ import {MaterialExampleModule} from './material.module';
     ReactiveFormsModule,
     MatNativeDateModule,
     HttpClientModule,
-    MaterialExampleModule
+    MaterialExampleModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
